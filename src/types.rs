bfe9db975d41316db732bfaec1903ae5ebd1f8da
l1l <khalil23ar@gmail.com>
2026-07-29 14:32:41 +0100

    Add Enemy && width and height to Player struct

--- src/types.rs
+++ src/types.rs
@@ -9,9 +9,22 @@ pub struct Bullet {
 }
 
 pub struct Player {
+    pub width: i32,
+    pub height: i32,
     pub x: f32,
     pub y: f32,
     pub speed: f32,
     pub mode: String,
     pub color: Color,
+}
+
+pub struct Enemy {
+    pub width: i32,
+    pub height: i32,
+    pub x: f32,
+    pub y: f32,
+    pub speed: f32,
+    pub color: Color,
+    pub active: bool,
+    pub mode: String,
 }
