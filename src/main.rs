ece8ae7492a98ca5480109c5dcabac8a55d3318a
l1l <khalil23ar@gmail.com>
2026-07-31 06:54:50 +0100

    Add game-over logic when an enemy touches the player

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
         x: 500.0,
         y: 550.0,
         speed: 0.03,
+        active: true,
         mode: "bullet".to_string(),
         color: Color::LIMEGREEN,
     };
@@ -26,7 +27,7 @@ fn main() {
 
     let mut rng = rand::rng();
     let mut enemy = Enemy {
-        width: 60,
+        width: 50,
         height: 30,
         x: rng.random_range(0..950) as f32,
         y: 0.0,
@@ -37,8 +38,6 @@ fn main() {
     };
 
     while !rl.window_should_close() {
-        rl.hide_cursor();
-
         if rl.is_key_down(KeyboardKey::KEY_UP) && player.y > 0.0 {
             player.y -= player.speed;
         }
@@ -62,14 +61,6 @@ fn main() {
         }
 
         let mut d: RaylibDrawHandle = rl.begin_drawing(&thread);
-        d.clear_background(Color::BLACK);
-        d.draw_text(
-            &format!("X: {:.0}, Y: {:.0}", player.x, player.y),
-            10,
-            10,
-            20,
-            Color::LIMEGREEN,
-        );
 
         if bullet.active {
             bullet.y -= bullet.speed;
@@ -88,11 +79,6 @@ fn main() {
                     && (bullet.x as u32 + enemy.width as u32)
                         >= (enemy.x as u32 + enemy.width as u32))
             {
-                println!(
-                    "Enemy x: {}, Enemy y: {}, Bullet x: {}, Bullet y: {}",
-                    enemy.x, enemy.y as u32, bullet.x, bullet.y as u32
-                );
-
                 enemy.color = Color::BLACK;
                 enemy.active = false;
             }
@@ -104,27 +90,71 @@ fn main() {
                 enemy.color = Color::BLACK;
             }
             enemy.y += enemy.speed;
+
+            println!("{} {}", enemy.y, player.y);
+
+            if enemy.y as i32 + player.height as i32 == player.y as i32
+                && (player.x as u32 <= (enemy.x as u32 + enemy.width as u32)
+                    && (player.x as u32 + enemy.width as u32)
+                        >= (enemy.x as u32 + enemy.width as u32))
+            {
+                player.active = false;
+            }
         }
 
-        // Draw Enemy
-        d.draw_rectangle(
-            enemy.x as i32,
-            enemy.y as i32,
-            enemy.width,
-            enemy.height,
-            enemy.color,
-        );
+        if player.active {
+            d.clear_background(Color::BLACK);
+            d.draw_text(
+                &format!("X: {:.0}, Y: {:.0}", player.x, player.y),
+                10,
+                10,
+                20,
+                Color::LIMEGREEN,
+            );
 
-        // Draw Bullet
-        d.draw_rectangle(bullet.x as i32, bullet.y as i32, 10, 10, bullet.color);
+            // Draw Enemy
+            d.draw_rectangle(
+                enemy.x as i32,
+                enemy.y as i32,
+                enemy.width,
+                enemy.height,
+                enemy.color,
+            );
 
-        // Draw Player
-        d.draw_rectangle(
-            player.x as i32,
-            player.y as i32,
-            player.width,
-            player.height,
-            player.color,
-        );
+            // Draw Bullet
+            d.draw_rectangle(bullet.x as i32, bullet.y as i32, 10, 10, bullet.color);
+
+            // Draw Player
+            d.draw_rectangle(
+                player.x as i32,
+                player.y as i32,
+                player.width,
+                player.height,
+                player.color,
+            );
+        } else {
+            d.clear_background(Color::RAYWHITE);
+            d.clear_background(Color::BLACK);
+            d.draw_text("GAME OVER", 10, 10, 40, Color::LIMEGREEN);
+        }
     }
 }
+
+/*
+fn create_enemies() {
+    let mut rng = rand::rng();
+    let i: u16 = rng.random_range(2..6) - 1;
+
+    let mut enemies: Vec<Enemy> = Vec::new();
+    while i > 0 {
+        enemies.push(Enemy {
+            x: rng.random_range(0..950) as f32,
+            y: 0.0,
+            speed: 0.009,
+            color: Color::BLACK,
+            active: true,
+            mode: "normal".to_string(),
+        });
+    }
+}
+*/
